3a7bac69a8c910f1f5efcdf168a859e7126ccd6e
alvin991 <alvin991@gmail.com>
2025-11-07 18:47:56 -0700

    Rename App2.jsx to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import App1 from './App1.jsx'
-import App2 from './App2.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App2 />
+    <App />
   </StrictMode>,
 )
