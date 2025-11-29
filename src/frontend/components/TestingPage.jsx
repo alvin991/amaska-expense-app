6b458c79a55fc35f1f49f3b4b450793a5d932872
alvin991 <alvin991@gmail.com>
2025-11-28 19:19:56 -0700

    Add react-router and TestingPage

---
+++ src/frontend/components/TestingPage.jsx
@@ -0,0 +1,12 @@
+import IconElement from "./IconElement";
+
+function TestingPage() {
+    return (
+        <div style={{ backgroundColor: 'lightgreen', height: '100vh' }}>
+            <h1>This is the Testing Page</h1>
+            <IconElement />
+        </div>
+    );
+}
+
+export default TestingPage;
--- src/frontend/main.jsx
+++ src/frontend/main.jsx
@@ -3,9 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import TestingPage from './components/TestingPage.jsx';
+
+const router = createBrowserRouter([
+    { path: "/", element: <App /> },
+    { path: "/testing", element: <TestingPage /> },
+  ],
+  { basename: import.meta.env.BASE_URL,}
+);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
