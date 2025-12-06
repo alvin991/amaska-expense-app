6e85b0ae99488bd0d499d1e9824ce62c3396263d
alvin991 <alvin991@gmail.com>
2025-12-06 00:11:04 -0700

    Remove unnecessary files

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,12 +1,10 @@
 import './App.css'
 import MyLayout from './components/MyLayout.jsx';
-import CategoryModal from './components/CategoryModal.jsx';
 
 function App() {
     return (
         <div className="App">
             <MyLayout />
-            {/* <CategoryModal /> */}
         </div>
     );
 }
